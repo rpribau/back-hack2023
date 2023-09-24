1ae2c1667a561ce11f6b037cd255c7f43481eddb
Rob <106797059+rpribau@users.noreply.github.com>
2023-09-23 19:19:28 -0600

    Main tests

---
+++ test.py
@@ -0,0 +1,25 @@
+# Importar la biblioteca de OpenAI
+import openai
+
+openai.api_type = "azure"
+openai.api_key = "6b25369971534252bbcee5e488ce59f1"
+openai.api_base = "https://openaistkinno.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"  # subject to change
+
+# Create a prompt
+prompt = "Once upon a time"
+
+# Create the completion
+response = openai.ChatCompletion.create(
+  engine="InnovationGPT2",
+  messages=[{
+      "role": "system",
+    "content": prompt
+  }],
+  max_tokens=5
+)
+
+# Print the output
+print(response.choices[0].message.content)
+
+
